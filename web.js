893c8cea26070994dd9d2f76568eee15fc6f3631
mShoult <mshoult@emlan.co.uk>
2014-05-21 11:52:29 +0100

    testing

--- web.js
+++ web.js
@@ -1,19 +1,21 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-var port;
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	port = Number(process.env.PORT || 5000);
-	res.send(port);
+	console.log('consolePort');
+	res.send(process.env.PORT);
+	res.send(process.env.PORT);
+	res.send(process.env.PORT);
+	res.send(process.env.PORT);
 });
 
-// var port = Number(process.env.PORT || 5000);
-// app.listen(port, function() {
-	// console.log("Listening on " + port);
-// });
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
 
 
 //var io = require('socket.io').listen(5000);
