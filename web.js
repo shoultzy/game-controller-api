0d62b0da28fdfbfa28343548fc5b2ab1a34dac9d
mShoult <mshoult@emlan.co.uk>
2014-05-21 11:48:02 +0100

    testing

--- web.js
+++ web.js
@@ -1,95 +1,93 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 	console.log('consolePort');
-  res.send(process.env.PORT);
+	res.send(port);
 });
 
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
-
-
 
 //var io = require('socket.io').listen(5000);
 //var mongoClient = require('mongodb').MongoClient;
 
 /*io.sockets.on('connection', function(socket) {
-	
-	console.log('onConnection');
-	
-	socket.emit('news', {
-		hello : 'world'
-	});
-	socket.on('my other event', function(data) {
-		console.log(data);
-	});
-});
 
-mongoClient.connect('mongodb://heroku_app25412544:ro9drd57j7r9qii6vh48fepgv9@ds049568.mongolab.com:49568/heroku_app25412544/messages_db', function(err, db) {
-	initCollection(db);
-});
+ console.log('onConnection');
 
-function initCollection(db) {
-	db.collectionNames('messages_collection', function(err, names) {
-		var collection;
+ socket.emit('news', {
+ hello : 'world'
+ });
+ socket.on('my other event', function(data) {
+ console.log(data);
+ });
+ });
 
-		if (names.length === 1) {
-			collection = db.collection('messages_collection');
+ mongoClient.connect('mongodb://heroku_app25412544:ro9drd57j7r9qii6vh48fepgv9@ds049568.mongolab.com:49568/heroku_app25412544/messages_db', function(err, db) {
+ initCollection(db);
+ });
 
-			verifyCollection(db, collection);
-		} else {
-			db.createCollection('messages_collection', {
-				capped : true,
-				size : 102400
-			}, function(err, collection) {
-				console.log('Created collection');
+ function initCollection(db) {
+ db.collectionNames('messages_collection', function(err, names) {
+ var collection;
 
-				verifyCollection(db, collection);
-			});
-		}
-	});
-}
+ if (names.length === 1) {
+ collection = db.collection('messages_collection');
 
-function initSockets(collection) {
-	io.sockets.on('connection', function(socket) {
-		var stream = collection.find({}, {
-			tailable : 1,
-			awaitdata : true,
-			numberOfRetries : -1
-		}).sort({
-			$natural : -1
-		}).stream();
+ verifyCollection(db, collection);
+ } else {
+ db.createCollection('messages_collection', {
+ capped : true,
+ size : 102400
+ }, function(err, collection) {
+ console.log('Created collection');
 
-		stream.on('data', function(data) {
-			socket.emit('messages', data);
-		});
+ verifyCollection(db, collection);
+ });
+ }
+ });
+ }
 
-		socket.on('add_message', function(data) {
-			collection.insert(data, function(err) {
-				console.log('Wrote', data);
-			});
-		});
-	});
-}
+ function initSockets(collection) {
+ io.sockets.on('connection', function(socket) {
+ var stream = collection.find({}, {
+ tailable : 1,
+ awaitdata : true,
+ numberOfRetries : -1
+ }).sort({
+ $natural : -1
+ }).stream();
 
-function verifyCollection(db, collection) {
-	collection.isCapped(function(err, capped) {
-		if (capped) {
-			initSockets(collection);
-		} else {
-			console.log('Not a capped collection');
+ stream.on('data', function(data) {
+ socket.emit('messages', data);
+ });
 
-			db.dropCollection('messages_collection', function(err) {
-				console.log('Dropped collection');
+ socket.on('add_message', function(data) {
+ collection.insert(data, function(err) {
+ console.log('Wrote', data);
+ });
+ });
+ });
+ }
 
-				initCollection(db);
-			});
-		}
-	});
-}*/
+ function verifyCollection(db, collection) {
+ collection.isCapped(function(err, capped) {
+ if (capped) {
+ initSockets(collection);
+ } else {
+ console.log('Not a capped collection');
+
+ db.dropCollection('messages_collection', function(err) {
+ console.log('Dropped collection');
+
+ initCollection(db);
+ });
+ }
+ });
+ }*/
